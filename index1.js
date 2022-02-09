4f09f5c6e57c881a0c24e798162ffd9c838012dd
kvparth <83730079+kvparth@users.noreply.github.com>
2022-02-10 03:43:19 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,39 @@
+"use strict";
+function convert(){
+    var from=document.getElementById("from").value;
+    var to=document.getElementById("to").value;
+    var fromValue=document.getElementById("frominput").value;
+    // document.getElementById("frominput").addEventListener("keypress",(e)=>{
+    //     if(e<from && e>=0){   
+    //     }
+    //     else{
+    //         alert(`enter value less than ${from}`);
+    //         document.getElementById("frominput").value=fromValue.slice(0,-1);
+    //     }        
+    // });
+    var toValue = (fromValue,from,to)=>{
+    return parseInt(fromValue,from).toString(to);
+}
+    document.getElementById("toinput").innerHTML=toValue(fromValue,from,to);
+    document.getElementById("result").style.display="block";
+    
+}
+function swap(){
+    var from=document.getElementById("from").value;
+    var to=document.getElementById("to").value;
+    let temp=from;
+    from=to;
+    to=temp;
+    document.getElementById("from").value=from;
+    document.getElementById("to").value=to;
+}
+document.getElementById("frominput").addEventListener("keypress",(e)=>{
+    if(e.key<from && e.key>=0){   
+    }
+    else{
+        alert(`enter value less than ${from}`);
+        document.getElementById("frominput").value=fromValue.slice(0,-1);
+    }        
+});
+
+
